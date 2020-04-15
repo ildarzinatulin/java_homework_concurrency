12ab0258fb1aec5991f20587c5ff35dab8282dea
ILdar Zinatulin <ildarzinatulin@MacBook-Air-ILdar.local>
2020-04-15 21:26:19 +0300

    task2 without Alien Method

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,29 +1,51 @@
 import java.util.concurrent.Callable;
 
 public class Task<T> {
+
+    private enum Status {
+        Finished, InProcess, DidntStart
+    }
+
     private final Callable<T> task;
-    private T result;
-    private TaskCallableException exception;
+    private volatile T result;
+    private volatile TaskCallableException exception;
+    private volatile Status firstThreadStatus = Status.DidntStart;
 
     public Task(Callable<T> callable) {
         task = callable;
     }
 
     public T get() throws Exception {
-        if (result != null) {
-            return result;
-        }
-        if (exception != null) {
-            throw exception;
-        }
+        Status status;
         synchronized (this) {
-            try {
-                result = task.call();
-            } catch (RuntimeException e) {
-                exception = new TaskCallableException("RuntimeException in Callable");
+            status = firstThreadStatus;
+            if (firstThreadStatus == Status.DidntStart) {
+                firstThreadStatus = Status.InProcess;
+            }
+        }
+
+        while (status == Status.InProcess) {
+            this.wait();
+            status = firstThreadStatus;
+        }
+
+        if (status == Status.Finished) {
+            if (result != null) {
+                return result;
+            } else {
                 throw exception;
             }
+        }
+
+        try {
+            result = task.call();
             return result;
+        } catch (Exception e) {
+            exception = new TaskCallableException("Exception in task");
+            throw exception;
+        } finally {
+            firstThreadStatus = Status.Finished;
+            this.notifyAll();
         }
     }
 }
